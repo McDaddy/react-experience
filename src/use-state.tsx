427089b09393cbc57a80a5f3409583a55bba453a
戣蓦 <mcdaddychen@126.com>
2020-08-05 16:01:48 +0800

    update

--- src/use-state.tsx
+++ src/use-state.tsx
@@ -3,12 +3,24 @@ import { Button } from "antd";
 
 export default () => {
   const [count, setCount] = React.useState(1);
+  const [count2, setCount2] = React.useState(1);
   const onClick = () => {
     setCount(count + 1);
   };
+
+  React.useEffect(() => {
+    if (count > 3) {
+      setCount2(state => state + 1);
+    }
+  }, [count]);
+
+  console.log(count2);
+
   return (
     <>
-      <div>测试useState {count}</div>
+      <div>
+        测试useState {count} {count2}
+      </div>
       <Button onClick={onClick}>click me</Button>
     </>
   );
